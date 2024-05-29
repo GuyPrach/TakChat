9850d19e48c5b18132814f0236b416d26b4c3e4a
Prach Trisiricharoen <guy.80647@gmail.com>
2024-05-29 12:15:00 +0700

    readme

--- screens/home/Home_page.js
+++ screens/home/Home_page.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView ,Text, View} from 'react-native';
 import Profile from '../../components/Home/Profile';
 
-export default function Home_page() {
+const Home_page = () => {
   return (
-    <View style={{flex:1, paddingTop:50}}>
-      <Profile />
+    // <ScrollView>
+    //   <View style = {{ flex : 1, paddingTop : 50}}>
+    //     <Profile />
+    //   </View>
+    // </ScrollView>
+
+  
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, This page will be Home page in the future!!</Text>
     </View>
   );
-}
+};
+export default Home_page;
